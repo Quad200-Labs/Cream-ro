5941650b72ce07c45dedf18db4aff4214d599419
Sachith Wickramasekara <101526693+SachithWickramasekara@users.noreply.github.com>
2025-05-10 01:30:09 +0530

    added font to the repo

--- app/components/molecules/text-block-hero.tsx
+++ app/components/molecules/text-block-hero.tsx
@@ -1,20 +1,22 @@
 import Button from "../atoms/button";
 import Heading from "../atoms/heading";
 import Text from "../atoms/text";
-
+import "../../styles/font.css";
 
 export default function HeroTextBlock() {
   return (
-    <div className="max-w-xl space-y-6">
-      <Heading>Delight in the Rich and Finest Flavors of CreamRo</Heading>
-      <Text>
+    <div className="sm:max-w-3xl space-y-6 relative">
+      <Heading className="text-3xl sm:text-6xl text-black w-full font-bold qurova-font">
+        Delight in the Rich and Finest Flavors of CreamRo
+      </Heading>
+      <Text className="max-w-md text-black font-semibold">
         Experience the magic of CreamRo – where every scoop is a masterpiece of
         rich, creamy delight. Made from the finest ingredients, our handcrafted
         flavors are designed to satisfy your sweetest cravings.
       </Text>
       <Button
         label="View more"
-        className="bg-black text-white hover:bg-gray-800"
+        className="bg-[#FFECD4] text-black hover:bg-[#FFF9EB] !font-bold mt-5 hover:border-2 hover:border-[#A68160] duration-300 border-2 border-[#FFECD4] hover:scale-95"
       />
     </div>
   );
--- app/components/organisms/home-page-sections/hero-section.tsx
+++ app/components/organisms/home-page-sections/hero-section.tsx
@@ -1,10 +1,21 @@
+import Image from "next/image";
 import HeroTextBlock from "../../molecules/text-block-hero";
 
 export default function Hero() {
   return (
-    <section className="bg-[#fdf6ee] flex flex-col-reverse lg:flex-row justify-between items-center px-8 py-16">
-      <HeroTextBlock />
-      <div className="w-[300px] h-[300px] bg-[#f5dcc0] rounded-xl"></div>
+    <section className="bg-[#FFF9EB] flex flex-col-reverse lg:flex-row justify-between items-center relative px-16 lg:px-28 xl:px-40 py-16 min-h-screen ">
+      <div className="relative z-10">
+        <Image
+          src="/icons/splash-icon-left.svg"
+          alt="Splash"
+          width={100}
+          height={100}
+          className="absolute -top-18 sm:-top-16 -left-12 lg:-left-16"
+        />
+        <HeroTextBlock />
+      </div>
+      <span className="creamro-text">CREAMRO</span>
+      <div className="w-[560px] h-[675px] bg-[#f5dcc0] rounded-xl hidden lg:block"></div>
     </section>
   );
 }
