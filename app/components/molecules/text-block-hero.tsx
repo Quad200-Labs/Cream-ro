67531cb8e11ab7c1d86f2a1071370fd404c4bc15
Sachith Wickramasekara <101526693+SachithWickramasekara@users.noreply.github.com>
2025-05-10 09:53:14 +0530

    added font to the repo

--- app/components/molecules/text-block-hero.tsx
+++ app/components/molecules/text-block-hero.tsx
@@ -5,15 +5,17 @@ import "../../styles/font.css";
 
 export default function HeroTextBlock() {
   return (
-    <div className="sm:max-w-3xl space-y-6 relative">
-      <Heading className="text-3xl sm:text-6xl text-black w-full font-bold qurova-font">
+    <div className="sm:max-w-2xl space-y-6 relative">
+      <Heading className="text-3xl sm:text-6xl text-black w-full font-semibold qurova-font">
         Delight in the Rich and Finest Flavors of CreamRo
       </Heading>
-      <Text className="max-w-md text-black font-semibold">
-        Experience the magic of CreamRo – where every scoop is a masterpiece of
-        rich, creamy delight. Made from the finest ingredients, our handcrafted
-        flavors are designed to satisfy your sweetest cravings.
-      </Text>
+      <div>
+        <Text className="max-w-md text-black font-semibold">
+          Experience the magic of CreamRo – where every scoop is a masterpiece
+          of rich, creamy delight. Made from the finest ingredients, our
+          handcrafted flavors are designed to satisfy your sweetest cravings.
+        </Text>
+      </div>
       <Button
         label="View more"
         className="bg-[#FFECD4] text-black hover:bg-[#FFF9EB] !font-bold mt-5 hover:border-2 hover:border-[#A68160] duration-300 border-2 border-[#FFECD4] hover:scale-95"
