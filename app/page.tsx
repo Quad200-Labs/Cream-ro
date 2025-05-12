adfabc20345028d667def495b086abdf0f652f9b
Sachith Wickramasekara <101526693+SachithWickramasekara@users.noreply.github.com>
2025-05-13 00:36:52 +0530

    Revert "home page is done"

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import FlavorsSection from "./components/organisms/home-page-sections/flavors-section";
 import Hero from "./components/organisms/home-page-sections/hero-section";
-import ProductsSection from "./components/organisms/home-page-sections/products-section";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <FlavorsSection />
-      <ProductsSection />
     </>
   );
 }
